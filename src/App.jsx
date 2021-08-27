d668c64cfb8118b6b8862f59ee0cc57aa32b59b3
iska07 <iskandrzh@gmail.com>
2021-08-26 15:55:48 -1200

    iska07

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Catalog from "./Pages/Catalog";
 import "./Style.css"
 // import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-<<<<<<< HEAD
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import { useState } from "react";
 import Header from "./Pages/header";
 // import GameList from "./Pages/GameList"
-=======
 import {useState} from "react";
->>>>>>> 236c87574fea2981d59a8695a8793775709744e7
+
 
 function App() {
   const [num, setNum] = useState(2)
