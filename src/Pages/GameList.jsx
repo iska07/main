105bb8e6d846eb42d86ff946dc3276ba859209a9
iska07 <iskandrzh@gmail.com>
2021-08-27 15:20:59 -1200

    iska07

--- src/Pages/GameList.jsx
+++ src/Pages/GameList.jsx
@@ -1,19 +1,32 @@
 import "../Style.css";
-import movie from "../movie.json";
+// import movie from "../movie.json";
 import { useState, useEffect } from "react";
 
 function GameList() {
   const link = "https://image.tmdb.org/t/p/w500";
 
-  const [movies, setMovies] = useState([]);
+  // const [movie, setMovie] = useState(null);
+
+  const [movie, setMovie] = useState([]);
 
   useEffect(() => {
-    fetch("GET https://www.freetogame.com/api/games")
+    fetch("https://www.freetogame.com/api/games" + id)
       .then((res) => res.json())
       .then((data) => {
-        setMovies(data.results);
+        setMovie(data);
       });
   }, []);
+
+  console.log
+
+  // fetch("https://www.freetogame.com/api/games")
+  // .then((response) => {
+  // response.text()
+  //   return response.json(); // Error!
+  // })
+  // .then((data) => {
+  //   console.log(data);
+  // });
 
   return (
     <div className="main">
@@ -22,14 +35,14 @@ function GameList() {
           <h1>Best Free to Play Games for PC and Browser in 2021!</h1>
           <span>351 free-to-play games found in our list!</span>
           <div className="mt-4">
-              <div className="game-card">
-                  <div className="card">
-                      <div className="image-wrapper"></div>
-                      <span></span>
-                      <video src=""></video>
-                      <a href=""></a>
-                  </div>
+            <div className="game-card">
+              <div className="card">
+                <div className="image-wrapper"></div>
+                <span></span>
+                <video src=""></video>
+                <a href=""></a>
               </div>
+            </div>
           </div>
         </div>
         <div className="card-list"></div>
